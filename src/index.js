c03e58c8b661946ea99cfad0afd4f26f9514d4ad
Velychko Daniil <101584452+Kl0filinj@users.noreply.github.com>
2022-12-23 00:18:11 +0100

    footer | card-form

--- src/index.js
+++ src/index.js
@@ -10,3 +10,27 @@
     refs.menu.classList.toggle('is-hidden');
   }
 })();
+
+(() => {
+  function maskCardCode(event) {
+    let vcc = this.value.replace(/\D/g, '');
+    this.value = '';
+    for (let i = 0; i < vcc.length; i++) {
+      this.value += (i % 4 == 0 && i != 0 ? ' ' : '') + vcc[i];
+    }
+  }
+  document
+    .getElementById('cardCode')
+    .addEventListener('input', maskCardCode, false);
+})();
+
+(() => {
+  function maskCC(event) {
+    let vcc = this.value.replace(/\D/g, '');
+    this.value = '';
+    for (let i = 0; i < vcc.length; i++) {
+      this.value += (i % 2 == 0 && i != 0 ? ' / ' : '') + vcc[i];
+    }
+  }
+  document.getElementById('startDate').addEventListener('input', maskCC, false);
+})();
