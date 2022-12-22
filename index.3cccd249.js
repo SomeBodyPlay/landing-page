678f61059c8b06e34b8712fb476fb603a13db9c2
Kl0filinj <101584452+Kl0filinj@users.noreply.github.com>
2022-12-22 11:43:58 +0000

    Deploying to gh-pages from @ Kl0filinj/lending-gui@49913fd340526edbd7bdacaa0fe4c1d08ec52ae5 🚀

---
+++ index.3cccd249.js
@@ -0,0 +1,2 @@
+(()=>{const e={openMenuBtn:document.querySelector("[data-menu-open]"),menu:document.querySelector("[data-menu]")};e.openMenuBtn.addEventListener("click",(function(){e.menu.classList.toggle("is-hidden")}))})();
+//# sourceMappingURL=index.3cccd249.js.map
---
+++ index.960b9391.js
@@ -0,0 +1,2 @@
+!function(){var e;(e={openMenuBtn:document.querySelector("[data-menu-open]"),menu:document.querySelector("[data-menu]")}).openMenuBtn.addEventListener("click",(function(){e.menu.classList.toggle("is-hidden")}))}();
+//# sourceMappingURL=index.960b9391.js.map
